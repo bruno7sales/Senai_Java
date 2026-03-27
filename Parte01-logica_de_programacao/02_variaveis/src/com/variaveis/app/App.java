25396cf2a25939c28553effa9dc2db0509f4bb83
Bruno. <bruno07sales@gmail.com>
2026-03-27 19:52:22 -0300

    Delete Parte01-logica_de_programacao directory

--- Parte01-logica_de_programacao/02_variaveis/src/com/variaveis/app/App.java
+++
@@ -1,8 +0,0 @@
-package com.variaveis.app;
-public class App {
-    public static void main(String[] args) throws Exception {
-       String nome;
-       nome = "Pablo Silva";
-        System.out.println(nome);
-    }
-}
