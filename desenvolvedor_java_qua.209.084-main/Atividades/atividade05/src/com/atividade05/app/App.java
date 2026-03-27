a80d13211cd603af95b500e252121e565133a8e2
Bruno. <bruno07sales@gmail.com>
2026-03-27 19:51:04 -0300

    Add files via upload

---
+++ desenvolvedor_java_qua.209.084-main/Atividades/atividade05/src/com/atividade05/app/App.java
@@ -0,0 +1,18 @@
+package com.atividade05.app;
+public class App {
+    public static void main(String[] args) throws Exception {
+        // potenciação (nesse caso, 4²)
+        System.out.println(Math.pow(4,2));
+
+        // número do π
+        System.out.println(Math.PI);
+    }
+}
+// TODO: atividade 05
+// Utilizando os conceitos básicos de orientação a objetos, crie um 
+// programa que um aluno informa seus dados (nome, idade e email) e pede 
+// para um professor (dados: nome e matrícula) calcular para ele as 
+// seguintes fórmulas: área do triângulo, área do círculo e equação do 1º 
+// grau, e exibe o resultado na tela.
+// NOTE: crie as classes Aluno e Professor no package models, que fica 
+// dentro de com.atividade05.
